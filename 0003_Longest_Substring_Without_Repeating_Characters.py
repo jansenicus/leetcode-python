70a45c50aa9ed89115e93263913cc9a6d3707e51
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-13 12:28:37 +0700

    Create 0003_Longest_Substring_Without_Repeating_Characters.py

---
+++ 0003_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        G = {}                                # let G be the map {element: greatest index}
+        g = 0                                 # let g be the index to increase greatness
+        delta = 0                             # let delta be the placeholder for distance
+        
+        for i in range(0, len(s)):            # loop from 0 to the length of string `s`
+            
+            e = s[i]                          # element `e` is the i-th array of s
+            if e in G:                        # if element already exists in map
+                g = max( G[e], g)             # we need to update `g` value to its maximum possible value
+                
+            delta = max(delta, i-g + 1)       # we need to update `delta` to its maximum possible value
+            G[e] = i + 1                      # update the index value of dictionary `G` for element `e`
+            
+        return delta                          # the final `delta` value is the length of longest subtring
+        
+        
+        
