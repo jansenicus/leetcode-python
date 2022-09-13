a2fd6d8438bce8e9580d6fd19ebb0cf8d02259c1
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-13 19:44:59 +0700

    Update 0011_Container_with_Most_Water.py

--- 0011_Container_with_Most_Water.py
+++ 0011_Container_with_Most_Water.py
@@ -17,12 +17,12 @@ class Solution:
 
         while left < right:
 			
-        	maxArea = max(maxArea, min( height[left], height[right] ) * ( right - left ))
-        	
-		if height[left] < height[right]:	
-        		left += 1
-        	else:
-        		right -= 1
+            maxArea = max(maxArea, min( height[left], height[right] ) * ( right - left ))
+            
+            if height[left] < height[right]:	
+                left += 1
+            else:
+                right -= 1
 
         return maxArea 
         
