820097f3ad37d0370d75654f3b9062a6f22e249f
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-13 16:34:29 +0700

    Update 0008_String_to_Integer_(atoi).py

--- 0008_String_to_Integer_(atoi).py
+++ 0008_String_to_Integer_(atoi).py
@@ -8,14 +8,14 @@ class Solution:
         s = re.match(regexp, s)        	# match the regex pattern 
         if not s: return 0              # return 0 if no string
 
-				maxint = 2147483647             
+	maxint = 2147483647             
         minint = -2147483648
 				
-        if s:														# if there is a match then do several checks
+        if s:				# if there is a match then do several checks
             num = int(s.group())
-            if num > maxint:						# if it is greater than maxint then return maxint
+            if num > maxint:		# if it is greater than maxint then return maxint
                 return maxint
-            elif num < minint:					# if is is lesser than minint then return minint
+            elif num < minint:		# if is is lesser than minint then return minint
                 return minint
-            else:												# when all checks failed then return `num` as is
+            else:			# when all checks failed then return `num` as is
                 return num
