936c49ebbe3aaa454aa0d9f7871ea98cdb4a5f4f
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-20 12:23:47 +0700

    Q: Quadruplets q: quadruplet qsum: sum(q)

--- 0018_4Sum.py
+++ 0018_4Sum.py
@@ -5,42 +5,37 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        quadruplets = []
-        nums.sort()
-		
-        for i in range(len(nums) - 3):												# last index on len(nums)-3
-			
-            if i > 0 and nums[i] == nums[i-1]:										# skip if current number equals previous number
+        Q = []                                                  # Quadruplets placeholder
+        nums.sort()                                             # sort first the array
+        n = len(nums)                                           # the length of the array
+
+        for i in range(n - 3):                                  # last index on len(nums)-3
+
+            if i > 0 and nums[i] == nums[i - 1]:                # skip if current number equals previous number
                 continue
-				
-            for j in range(i+1, len(nums)-2):										# next number loop begins with i+1 to len(nums)-2
-				
-                l, r = j + 1, len(nums) - 1											# two pointers l and r, begins with j+1 to len(nums)-1
-				
-                while l < r:														# while l < r then keep looping
-					
-					quadruplet = [ nums[i], nums[j], nums[l], nums[r] ]				# quadruplet
-					
-                    Sum = sum(quadruplet)											# Sum of quadruplet
-					
-                    if Sum < target:												# if Sum less than target then increase the `l` index
+
+            for j in range(i + 1, n - 2):                       # next number loop begins with i+1 to len(nums)-2
+
+                l, r = j + 1, n - 1                             # two pointers l and r, begins with j+1 to len(nums)-1
+
+                while l < r:                                    # while l < r then keep looping
+
+                    q = (nums[i], nums[j],
+                         nums[l], nums[r])                      # quadruplet
+
+                    qsum = sum(q)                               # Sum of quadruplet
+
+                    if qsum < target:                           # if qsum < target -> increase the `l` index
                         l += 1
-						
-                    elif Sum > target:												# if Sum greater than target then decrease the `r` index
+
+                    elif qsum > target:                         # if qsum > target -> decrease the `r` index
                         r -= 1
-						
+
                     else:
-                        quadruplets.append( quadruplet )			
-                        l += 1
-						
-                        while nums[l] == nums[l-1] and l < r :						# keep skipping if current number equals previous number
-                            l += 1													# increase `l` index
-							
-        Quadruplets = []															# Unique Quadruplets
-		
-        for quadruplet in list(set(quadruplets)):					
-            Quadruplets.append(list(quadruplet))
-			
-		Quadruplets = list(set(quadruplets)
-			
-        return Quadruplets
+                        Q.append(q)                             # add quadruplet into Quadruplets
+                        l += 1                                  # increase the `l` index
+
+                        while nums[l] == nums[l-1] and l<r:     # keep skipping if current number equals previous number
+                            l += 1                              # increase `l` index
+
+        return list(set(Q))                                     # unique set of Quadruplets
