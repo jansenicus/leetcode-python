b6d8cc651ae1ecdb015823479a353cae2cf7f3b9
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-24 21:13:04 +0700

    added problems to solve

---
+++ 0020 Valid Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        """
+        https://leetcode.com/problems/valid-parentheses/
+        https://youtu.be/WTzjTskDFMg
+
+        Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+        determine if the input string is valid.
+
+        An input string is valid if:
+
+        Open brackets must be closed by the same type of brackets.
+        Open brackets must be closed in the correct order.
+        Every close bracket has a corresponding open bracket of the same type.
+
+        """
