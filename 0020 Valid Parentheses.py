239adaaa52ae565073e9f0a31574a3138fb3466f
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-26 14:44:20 +0700

    added Valid Parentheses solution
    using stack to store every opening element;
    the last opening element in the stack should pair with the last element checked otherwise return False

--- 0020 Valid Parentheses.py
+++ 0020 Valid Parentheses.py
@@ -14,3 +14,31 @@ class Solution:
         Every close bracket has a corresponding open bracket of the same type.
 
         """
+
+        if len(s) % 2:
+            return False
+
+        pair = {                                    # pair = {open: close}
+            '(': ')',                               # opening and closing pairs of parentheses
+            '{': '}',
+            '[': ']'
+        }
+
+        stack = []                                  # stack to store every opening elements in `s`
+
+        for _ in s:                                 # loop for every element in `s`
+
+            if _ in pair.keys():                    # if current element is an opening element
+                stack.append(_)                     # put current opening element into the stack
+
+            else:                                   # otherwise current element is a closing element
+
+                if not stack:                       # check if stack is empty
+                    return False                    # which means no opening element available
+
+                last_opening = stack.pop()          # retrieve last opening element in the stack and remove it
+
+                if pair[last_opening] != _:         # the pair for last opening element in the stack should
+                    return False                    # equal the current closing element, otherwise return False
+
+        return stack == []                          # opening elements stack must be empty due to pop removal
