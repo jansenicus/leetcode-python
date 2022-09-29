14ecbaf56dbf69bbaa02c153b8c89a617ec18940
Jansen A. Simanullang <jansen.simanullang@gmail.com>
2022-09-29 17:02:53 +0700

    added solutions for Merge Two and k Sorted List

--- 0021 Merge Two Sorted Lists.py
+++ 0021 Merge Two Sorted Lists.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         """
@@ -33,3 +33,16 @@ class Solution:
         -100 <= Node.val <= 100
         Both list1 and list2 are sorted in non-decreasing order.
         """
+        prev = dummy = ListNode(None)
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                prev.next = list1
+                list1 = list1.next
+            else:
+                prev.next = list2
+                list2 = list2.next
+            prev = prev.next
+        prev.next = list1 or list2
+
+        return dummy.next
--- 0023 Merge k Sorted Lists.py
+++ 0023 Merge k Sorted Lists.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         """
@@ -40,3 +40,24 @@ class Solution:
         lists[i] is sorted in ascending order.
         The sum of lists[i].length will not exceed 104.
         """
+        import heapq
+
+        prev = dummy = ListNode(None)
+        next_nodes, heap = [], []
+
+        for i, node in enumerate(lists):
+            next_nodes.append(node)
+            if node:
+                heap.append((node.val, i))
+        heapq.heapify(heap)
+
+        while heap:
+            value, i = heapq.heappop(heap)
+            node = next_nodes[i]
+            prev.next = node
+            prev = prev.next
+            if node.next:
+                next_nodes[i] = node.next
+                heapq.heappush((heap, (node.next.val, i)))
+
+        return dummy.next
